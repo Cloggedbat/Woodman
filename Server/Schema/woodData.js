b7a544980e377635156cacac45c09997996f9fa2
cloggedbat <alvinclemens@gmail.com>
2021-06-08 15:15:03 -0600

    first

---
+++ Server/Schema/woodData.js
@@ -0,0 +1,20 @@
+// const mongoose = require("mongoose");
+// const { kStringMaxLength } = require("buffer");
+// require('../config/db')
+// require('../models/dbM')
+// var Schema = mongoose.Schema;
+
+
+// const WoodSchema = new Schema({
+//     name: { type: String, required: false },
+//     body: { type: String, required: false },
+//     date: {
+//         type: String,
+//         default: Date.now()
+//     }
+
+// });
+
+// const woodD = mongoose.model('woodData', WoodSchema)
+
+// module.exports = woodD;
---
+++ Server/models/woodData.js
@@ -0,0 +1,3 @@
+module.exports = {
+    woodModle: require("./woodD")
+};
